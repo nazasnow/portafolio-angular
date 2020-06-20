fa2bb310c3a54979a8cf899f1d7025b39b88a74c
Naza Rodriguez <nrrodriguez.1991@gmail.com>
2020-06-20 16:27:44 +0200

    Fin apartado 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,38 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+
+  private cargarEquipo() {
+
+      //Leer el archivo JSON
+      this.http.get('https://angular-htlm.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        
+        this.equipo = resp;
+        console.log(resp);
+    });
+  }
+
+  private cargarInfo() {
 
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
+    .subscribe( (resp: InfoPagina) => {
+      
+      this.cargada = true;
+      this.info = resp;
+      console.log(resp);
+    });
 
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
 
   }
+
 }
